12da4a37aa852a77738b6a07de6aa990842809ec
saumyaranjan717 <saumyar755@gmail.com>
2026-07-17 16:21:09 +0530

    Added Task 6: Secure File Deletion

--- tasks/task6_secure_file_deletion/task6.c
+++ tasks/task6_secure_file_deletion/task6.c
@@ -1,3 +1,17 @@
-/*
- * Task 6 - Secure File Deletion
- */
+void perform_task6(){
+char filename[50];
+char username[50];
+printf("Task 6: Secure File Deletion\n");
+printf("enter username-");
+scanf("%s",username);
+printf("enter file to be deleted-");
+scanf("%s",filename);
+if(remove(filename) == 0){
+printf("file deleted successfully\n");
+}
+else{
+printf("file not deleted\n");
+}
+return;
+}
+
